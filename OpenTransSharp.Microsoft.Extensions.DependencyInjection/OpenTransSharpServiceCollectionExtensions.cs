6c4a96cda7a419c24a7db04183f896841ca39357
warappa <spoiltnr1@gmx.at>
2021-02-25 04:49:31 +0100

    Microsoft.Extensions.DependencyInjection integration

---
+++ OpenTransSharp.Microsoft.Extensions.DependencyInjection/OpenTransSharpServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using OpenTransSharp;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OpenTransSharpServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOpenTransSharp(this IServiceCollection services, Action<OpenTransOptions> configureOptions = null)
+        {
+            services.AddSingleton<IOpenTransXmlSerializerFactory, OpenTransXmlSerializerFactory>();
+            services.AddOptions<OpenTransOptions>();
+            services.AddTransient(sp => sp.GetRequiredService<IOptions<OpenTransOptions>>().Value);
+
+            if (configureOptions is object)
+            {
+                services.Configure(configureOptions);
+            }
+
+            return services;
+        }
+    }
+}
