fba3168cfafa8374589397746ffcfe53e168aa48
warappa <spoiltnr1@gmx.at>
2021-06-19 20:52:26 +0200

    Added ASP.NET Core orders integration tests

---
+++ OpenTransSharp.Microsoft.AspNetCore.Tests/Orders/OrdersIntegrationTests.cs
@@ -0,0 +1,128 @@
+using OpenTransSharp.Samples.AspNetCore;
+using OpenTransSharp.Validation;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace OpenTransSharp.Microsoft.AspNetCore.Tests.Orders
+{
+    public class OrdersIntegrationTests
+    {
+        private WebApplicationFactory<Startup> factory;
+        private HttpClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            factory = new WebApplicationFactory<Startup>();
+            client = factory.CreateClient();
+        }
+
+        [Test]
+        public async Task Can_parse_valid_Invoice_via_stream()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature.xml");
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
+
+            var result = await client.PostAsync("OrderValidation/via-stream", content);
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task Cannot_parse_invalid_Invoice_via_stream()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature - invalid.xml");
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
+
+            var result = await client.PostAsync("OrderValidation/via-stream", content);
+            result.StatusCode.Should().Be(400);
+
+            var error = await GetValidationResult(result);
+            error.Errors.First().Value[0].Should().Contain("MaxLength");
+        }
+
+        [Test]
+        public async Task Can_parse_valid_Invoice_via_model_binding()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature.xml");
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/xml")
+            {
+                CharSet = Encoding.GetEncoding("iso-8859-1").WebName
+            };
+
+            var result = await client.PostAsync("OrderValidation/via-model-binding", content);
+            var r = await result.Content.ReadAsStringAsync();
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task Cannot_parse_invalid_Invoice_via_model_binding()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature - invalid.xml");
+
+            var content = new StreamContent(stream);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/xml");
+
+            var result = await client.PostAsync("OrderValidation/via-model-binding", content);
+            result.StatusCode.Should().Be(400);
+
+            var error = await GetValidationResult(result);
+            error.Errors.ElementAt(1).Value[0].Should().Contain("MaxLength");
+        }
+
+        [Test]
+        public async Task Can_parse_valid_Invoice_via_file()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature.xml");
+
+            var content = new MultipartFormDataContent
+            {
+                {new StreamContent(stream), "file", @"Orders\sample_order_opentrans_2_1_xml signature.xml"}
+            };
+
+            var result = await client.PostAsync("OrderValidation/via-file", content);
+            var r = await result.Content.ReadAsStringAsync();
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task Cannot_parse_invalid_Invoice_via_file()
+        {
+            using var stream = File.OpenRead(@"Orders\sample_order_opentrans_2_1_xml signature - invalid.xml");
+
+            var content = new MultipartFormDataContent
+            {
+                {new StreamContent(stream), "file", @"Orders\sample_order_opentrans_2_1_xml signature - invalid.xml"}
+            };
+
+            var result = await client.PostAsync("OrderValidation/via-file", content);
+            result.StatusCode.Should().Be(400);
+
+            var error = await GetValidationResult(result);
+            error.Errors.First().Value[0].Should().Contain("MaxLength");
+        }
+
+        private static async Task<ValidationResult> GetValidationResult(HttpResponseMessage result)
+        {
+            var errorJson = await result.Content.ReadAsStringAsync();
+            var error = JsonSerializer.Deserialize<ValidationResult>(errorJson, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            return error;
+        }
+    }
+}
